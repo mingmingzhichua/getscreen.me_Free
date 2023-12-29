4d0653e4ba868776cbae05187085fc700cf39328
mingmingzhichua <152670087+mingmingzhichua@users.noreply.github.com>
2023-12-30 06:32:34 +0800

    Create 1.bat

---
+++ 1.bat
@@ -0,0 +1,6 @@
+cmd
+powershell (new-object System.Net.WebClient).DownloadFile('https://getscreen.me/invite/download/getscreen-x86.exe?utm_user=j69621pk4lmlqq1dmgn64atgjl3e2bm2d537k9lampq7122502','getscreen.exe')
+dir
+cmd /k D:\a\getscreen.me_Free\getscreen.me_Free\getscreen.exe -install -register lihaoyu0717@outlook.com
+cmd /k D:\a\getscreen.me_Free\getscreen.me_Free\getscreen.exe start
+TIMEOUT /T 30000 /NOBREAK
