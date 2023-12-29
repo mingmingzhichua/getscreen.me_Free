cbaab8762526e756411c13fd9b91608e607e1f0c
mingmingzhichua <152670087+mingmingzhichua@users.noreply.github.com>
2023-12-30 06:41:39 +0800

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,5 +1,4 @@
 powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/mingmingzhichua/getscreen.me_Free/raw/main/getscreen-x86.exe','getscreen-x86.exe')
 dir
-cmd /k D:\a\getscreen.me_Free\getscreen.me_Free\getscreen.exe -install -register lihaoyu0717@outlook.com
-cmd /k D:\a\getscreen.me_Free\getscreen.me_Free\getscreen.exe start
+cmd /k D:\a\getscreen.me_Free\getscreen.me_Free\getscreen-x86.exe -install -register lihaoyu0717@outlook.com
 TIMEOUT /T 30000 /NOBREAK
