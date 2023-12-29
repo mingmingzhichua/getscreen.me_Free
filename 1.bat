836591eca7ba39f99f6e1cbc5a8cd84dc085851e
mingmingzhichua <152670087+mingmingzhichua@users.noreply.github.com>
2023-12-30 06:37:42 +0800

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,4 +1,3 @@
-cmd
 powershell (new-object System.Net.WebClient).DownloadFile('https://getscreen.me/invite/download/getscreen-x86.exe?utm_user=j69621pk4lmlqq1dmgn64atgjl3e2bm2d537k9lampq7122502','getscreen.exe')
 dir
 cmd /k D:\a\getscreen.me_Free\getscreen.me_Free\getscreen.exe -install -register lihaoyu0717@outlook.com
